2c63c46d48514cd8aaf7422b86066298c4342723
Hein Busger op Vollenbroek <heinbusgeropv@MacBook-Air-van-Hein.local>
2020-11-19 18:06:03 +0100

    Week 2 exercise

--- Week_1/hello.py
+++ Week_1/hello.py
@@ -1,2 +1,14 @@
 print("Hello World!")
 
+# Exercise 1.12
+print("hi Im Hein & # pleased to meet you ")
+print("It's cold as hell here")
+
+1+2
+2*2
+
+print("hello")
+"hello"
+
+6+4*9
+print(6+4*9)
